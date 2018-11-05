44bd1f642487f4494c1b07ecf27d0719a60adb03
buo2107 <vampire91145@gmail.com>
2018-11-05 19:35:41 +0800

    add move funtion

--- Tom&Jerry/ViewController.swift
+++ Tom&Jerry/ViewController.swift
@@ -14,36 +14,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var jerryImage: UIImageView!
     @IBOutlet weak var earthImage: UIImageView!
     @IBOutlet weak var distanceSlider: UISlider!
-    let trans = CGAffineTransform(rotationAngle: -CGFloat.pi/4)
+    let trans = CGAffineTransform(rotationAngle: -CGFloat.pi/5.3)
     let tomX = 67.5
     let tomY = 318.5
-    
+    var tomCenter = CGPoint()
+    var jerryCenter = CGPoint()
+    var earthCenter = CGPoint()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tomImage.transform = trans
         earthImage.rotate360Degree()
+        
+        tomImage.center = CGPoint(x: 67.5, y: 375)
+        jerryImage.center = CGPoint(x: 623, y: 375)
+        tomCenter = tomImage.center
+        jerryCenter = jerryImage.center
+        earthCenter = earthImage.center
     }
     
     @IBAction func distanceChange(_ sender: UISlider) {
+        let radian = Double(5)*Double.pi/180
+        tomImage.center = CGPoint(x: Double(earthCenter.x) - 275*cos(radian*Double(sender.value)), y: Double(earthCenter.y) - 275*sin(radian*Double(sender.value)))
+        jerryImage.center = CGPoint(x: Double(earthCenter.x) + 275*cos(radian/2*Double(sender.value)), y: Double(earthCenter.y) - 275*sin(radian/2*Double(sender.value)))
         
+        if(sender.value == 23)
+        {
+            earthImage.stopRotate()
+        }
+ 
     }
     
 }
 
 extension UIImageView {
     
-    // 360度旋转图片
+    // 360度旋轉圖片
     func rotate360Degree() {
-        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z") // 让其在z轴旋转
-        rotationAnimation.toValue = NSNumber(value: Double.pi * 2.0) // 旋转角度
-        rotationAnimation.duration = 3.0 // 旋转周期
-        rotationAnimation.isCumulative = true // 旋转累加角度
-        rotationAnimation.repeatCount = 100000 // 旋转次数
+        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z") // 在z軸旋轉
+        rotationAnimation.toValue = NSNumber(value: Double.pi * 2.0) // 旋轉角度
+        rotationAnimation.duration = 3.0 // 旋轉周期
+        rotationAnimation.isCumulative = true // 旋轉累加角度
+        rotationAnimation.repeatCount = 100000 // 旋轉次數
         layer.add(rotationAnimation, forKey: "rotationAnimation")
     }
     
-    // 停止旋转
+    // 停止旋轉
     func stopRotate() {
         layer.removeAllAnimations()
     }
